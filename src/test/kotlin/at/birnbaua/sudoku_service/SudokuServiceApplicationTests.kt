34c6fd66a80a1efecd11e1551a0144db29a98af7
chrigHub <51883441+chrigHub@users.noreply.github.com>
2021-10-21 14:14:50 +0200

    Creating webservice orphan branch

---
+++ src/test/kotlin/at/birnbaua/sudoku_service/SudokuServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package at.birnbaua.sudoku_service
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SudokuServiceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
