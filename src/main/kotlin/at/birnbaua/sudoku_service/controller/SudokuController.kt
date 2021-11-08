fb85f8539e7149bc1828c1a513db8c60baaa132a
Birnbaua <andreas.bachl@24speed.at>
2021-11-08 20:07:45 +0100

    added crossorigin functionality

--- src/main/kotlin/at/birnbaua/sudoku_service/controller/SudokuController.kt
+++ src/main/kotlin/at/birnbaua/sudoku_service/controller/SudokuController.kt
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/sudoku")
 class SudokuController {
