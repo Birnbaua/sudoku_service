93928e8808aa9d728e87fa31835a2a498979e9b2
chrigHub <51883441+chrigHub@users.noreply.github.com>
2021-12-31 00:42:29 +0100

    Fixed Timer

--- src/app/pages/play/play.component.ts
+++ src/app/pages/play/play.component.ts
@@ -6,10 +6,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./play.component.css']
 })
 export class PlayComponent implements OnInit {
-
   constructor() { }
 
+  time: number = 0;
+  display: string = "00:00:00";
+  interval: any;
+  timerOn: boolean = false;
+
   ngOnInit(): void {
+    this.startTimer()
   }
 
+  startTimer(){
+    console.log("PlayComponent: started timer")
+    this.timerOn = true;
+    this.interval = setInterval(() => {
+      this.time++;
+      this.display=this.transform(this.time)
+    }, 1000);
+  }
+
+  transform(value: number): string{
+    const minutes: number = Math.floor(value / 60);
+    const hours: number = Math.floor(value / 3600);
+    let time: string = ""
+    if(hours < 10 ){
+      time = time + 0 
+    }
+    time = time + hours + ':'
+    if(minutes - hours * 60 < 10){
+      time = time + 0 
+    }
+    time = time + (minutes - hours * 60) + ':'
+    if(value - minutes * 60 < 10){
+      time = time + 0
+    }
+    time = time  + (value - minutes * 60) 
+    return time;
+  }
+
+  toggleTimer() {
+    if(this.timerOn){
+      clearInterval(this.interval);
+      this.timerOn = !this.timerOn 
+    } else {
+      this.startTimer()
+    }
+  }
 }
